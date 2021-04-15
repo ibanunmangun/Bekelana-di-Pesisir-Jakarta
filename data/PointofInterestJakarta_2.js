3bad83a0f032af03706c1ac0becdb19d57ca2719
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-04-15 22:20:56 +0700

    Add files via upload

---
+++ data/PointofInterestJakarta_2.js
@@ -0,0 +1,21 @@
+var json_PointofInterestJakarta_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterestJakarta_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Taman Wisata Alam Mangrove", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.735584, -6.105334 ] } },
+{ "type": "Feature", "properties": { "name": "Kota Tua Jakarta", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8137856, -6.1347494 ] } },
+{ "type": "Feature", "properties": { "name": "Jakarta History Museum", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8133, -6.1352 ] } },
+{ "type": "Feature", "properties": { "name": "Jakarta Bahari Museum", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8085899, -6.1269553 ] } },
+{ "type": "Feature", "properties": { "name": "Gerbang Sunda Kelapa (ke Batavia Marina)", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8105964, -6.1253916 ] } },
+{ "type": "Feature", "properties": { "name": "Pasar Ikan Modern Muara Baru", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8015795, -6.1103679 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pluit Selatan", "X": 106.801304, "Y": -6.125491, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.801304, -6.125491 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Bahrul Ulum", "X": 106.802002, "Y": -6.110758, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.802002, -6.110758 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Penjaringan", "X": 106.800836, "Y": -6.12492, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.800836, -6.12492 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Muara Baru", "X": 106.804018, "Y": -6.121854, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.804018, -6.121854 ] } },
+{ "type": "Feature", "properties": { "name": "Parkir Kota Tua - Taman Kota Intan", "X": 106.811846, "Y": -6.131328, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.811846, -6.131328 ] } },
+{ "type": "Feature", "properties": { "name": "Parkir Kota Tua - 1001 Hotel Colosseum Club", "X": 106.814036, "Y": -6.132832, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.814036, -6.132832 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Tzu Chi", "X": 106.739296, "Y": -6.106354, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.739296, -6.106354 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Pluit", "X": 106.799541, "Y": -6.125728, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.799541, -6.125728 ] } }
+]
+}
